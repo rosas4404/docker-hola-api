88aaba0cebfbf576092a7fd19fafc83720228a90
rosas4404 <rosas4404@gmail.com>
2026-06-29 13:31:30 -0600

    Add files via upload

---
+++ src/test/java/com/example/dockerholaapi/DockerHolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerholaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHolaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
